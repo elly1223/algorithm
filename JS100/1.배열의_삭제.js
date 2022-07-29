5657b3268ab71b06986657f6db8b533ddcbfa452
elly1223 <poew91@gmail.com>
2022-07-29 15:24:07 +0900

    day 1

---
+++ JS100/1.배열의_삭제.js
@@ -0,0 +1,14 @@
+// 문제 1. 다음 배열에서 400, 500을 삭제하는 code를 입력하세요.
+// var nums = [100, 200, 300, 400, 500];
+
+
+// - 나의 풀이
+function test() {
+  let nums = [100, 200, 300, 400, 500]
+	nums.pop()
+	nums.pop()
+  
+  return nums
+}
+
+test()
---
+++ JS100/3.변수의_타입.js
@@ -0,0 +1,13 @@
+// 문제 3. 다음 출력 값으로 올바른 것은?
+// var arr = [100, 200, 300];
+// console.log(typeof(arr));
+// 1)  undefined
+// 2)  string
+// 3)  number
+// 4)  object
+
+
+// - 나의 풀이
+// 답 : 4번
+// array(배열)은 객체 타입이다. JS는 원시 타입(primitive type), 객체 타입(object / reference type)이 있다.
+// 나머지 1, 2, 3번은 원시 타입이다.
