c5cb1c701745caf01ace68912b15b85202eb9363
elly1223 <poew91@gmail.com>
2022-08-04 12:03:04 +0900

    day 4

---
+++ JS100/11.for를_이용한_기본_활용.js
@@ -0,0 +1,22 @@
+// 문제 11. for를 이용한 기본 활용
+
+// 1부터 100까지 모두 더하는 Code를 <pass> 부분에 완성하세요. `for`를 사용해야 합니다.
+// let s = 0;
+
+// //pass
+
+// console.log(s);
+
+
+// - 나의 풀이
+function test() {
+  let sum = 0
+
+  for (let i = 1; i <= 100; i++){
+    sum += i
+  }
+
+	console.log(sum);
+}
+
+test()
